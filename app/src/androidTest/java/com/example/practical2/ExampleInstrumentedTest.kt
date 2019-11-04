ca8d351da69272bc8adbdaa4da4621ff77591b25
njy <ngjy-wm17@student.tarc.edu.my>
2019-11-04 13:06:43 +0800

    2.1,2.2

--- app/src/androidTest/java/com/example/practical2/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/practical2/ExampleInstrumentedTest.kt
@@ -21,4 +21,5 @@ class ExampleInstrumentedTest {
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.practical2", appContext.packageName)
     }
+
 }
